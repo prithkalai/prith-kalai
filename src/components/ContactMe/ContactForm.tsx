c986a8491358281b6a747ae8b293c99cacaacae0
Prithviraj Kalaiselvan <prithviraj792000@gmail.com>
2024-03-08 00:45:03 -0500

    Feature: Added EmailJS service for contact form.

--- src/components/ContactMe/ContactForm.tsx
+++ src/components/ContactMe/ContactForm.tsx
@@ -1,34 +1,65 @@
-// TODO: Setup EmailJS client here to get form submissions
 //TODO: Perform Input Validation
+// TODO: Return form submission confirmation or failure to user (Toast Component)
+
+import { FormEvent, useRef } from "react";
+import emailJS from "@emailjs/browser";
 
 export const ContactForm = () => {
+  const formRef = useRef<HTMLFormElement>(null);
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (formRef.current) {
+      emailJS
+        .sendForm("service_x6i55ad", "template_258sbtj", formRef.current, {
+          publicKey: "zp5qYAfMbSPbUy9Og",
+        })
+        .then(
+          () => {
+            console.log("SUCCESS!");
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
+      formRef.current.reset();
+    }
+  };
+
   return (
     <div className="w-full flex justify-center">
-      <div className="flex flex-col  font-poppins w-[350px] sm:w-[450px] h-fit rounded-b-xl lg:rounded-xl  p-6 bg-yellow-400">
-        <p className="text-lg">Name</p>
+      <form
+        ref={formRef}
+        onSubmit={onSubmit}
+        className="flex flex-col  font-poppins w-[350px] sm:w-[450px] h-fit rounded-b-xl lg:rounded-xl  p-6 bg-yellow-400"
+      >
+        <label className="text-lg">Name</label>
         <input
           type="text"
           className="border-2 border-black mb-3 p-1 rounded-lg"
           placeholder="Enter Your Name"
+          name="name"
         ></input>
-        <p className="text-lg">Email</p>
+        <label className="text-lg">Email</label>{" "}
         <input
-          type="text"
+          type="email"
           className="border-2 border-black mb-3 p-1 rounded-lg"
           placeholder="Enter Your Email ID"
+          name="email"
         ></input>
-        <p className="text-lg">Message</p>
+        <label className="text-lg">Message</label>{" "}
         <input
           type="textarea"
           className="border-2 border-black h-32 mb-4 p-1 rounded-lg"
           placeholder="Enter your message to me"
+          name="message"
         ></input>
         <div className="w-full flex flex-row justify-center">
           <button className="flex flex-row items-center w-fit gap-2 p-1 sm:p-2 border-2 hover:text-white border-blue-900 hover:bg-blue-900 transition-colors text-sm rounded-2xl">
             Submit
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
